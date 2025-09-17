620383be981f1d6585e9afde5d5f6c66aa248c68
Candy-pt <126772019+Candy-pt@users.noreply.github.com>
2025-09-17 21:58:18 +0700

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,8 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', '0ach-7L4clBYZ_MSXXUI2mvcEJTCFmWPr0f-cFrv2Ek')
    
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:77866@localhost/hr_management"
+    SQLALCHEMY_DATABASE_URI = "mysql://root:GmHMiIeTHcjulYPITYZRKuaglPhuzodL@centerbeam.proxy.rlwy.net:34701/railway"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     FLASK_ENV = os.getenv('FLASK_ENV', 'development')
     FLASK_DEBUG = os.getenv('FLASK_DEBUG', '1')
+
